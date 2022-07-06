97c3e1bc07818f165b394d8fde8d545db565e5e3
Vincent FINET <vfinet@gmail.com>
2022-07-06 15:59:23 +0200

    Push the generated artefact into default org #159

--- build/build.sh
+++ build/build.sh
@@ -8,6 +8,7 @@
 
 ## uglifyjs javascript/src/OrgCheck.js -o force-app/main/default/staticresources/OrgCheck_OrgCheck_SR.resource
 uglifyjs --ie --webkit --v8 build/src/javascript/OrgCheck.js -o force-app/main/default/staticresources/OrgCheck_OrgCheck_SR.resource
+sfdx force:source:deploy -m StaticResource:OrgCheck_OrgCheck_SR
 
 ## https://codeinthehole.com/tips/tips-for-using-a-git-pre-commit-hook/
 ## ln -s ../../pre-commit.sh .git/hooks/pre-commit
@@ -17,12 +18,19 @@ uglifyjs --ie --webkit --v8 build/src/javascript/OrgCheck.js -o force-app/main/d
 
 
 ### -----------------------
-### LABELS AND TN18 PART
+### LABELS PART
 ### -----------------------
 
 cat build/src/labels/CustomLabels-copyandpasted.txt \
     | sed -e 's/""/"/g' -e 's/^"//' -e 's/"$//' \
     > force-app/main/default/labels/CustomLabels.labels-meta.xml
+
+sfdx force:source:deploy -m CustomLabels
+
+
+### -----------------------
+### TRANSLATIONS PART
+### -----------------------
 
 cat build/src/labels/Translation-FR-copyandpasted.txt \
     | sed -e 's/""/"/g' -e 's/^"//' -e 's/"$//' \
@@ -32,3 +40,4 @@ cat build/src/labels/Translation-JP-copyandpasted.txt \
     | sed -e 's/""/"/g' -e 's/^"//' -e 's/"$//' \
     > force-app/main/default/translations/ja.translation-meta.xml
 
+sfdx force:source:deploy -m Translations
