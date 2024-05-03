e5cc4e1f4d638037bca704f00b207251a35d7e27
Vincent FINET <vfinet@salesforce.com>
2024-05-03 18:18:33 +0200

    refresh if cache is more than one day #379

--- force-app/main/default/lwc/orgcheckApp/api/core/orgcheck-api-datacache.js
+++ force-app/main/default/lwc/orgcheckApp/api/core/orgcheck-api-datacache.js
@@ -8,10 +8,14 @@ const generateLogicalKey = (key) => {
     return key.startsWith(CACHE_PREFIX) ? key.substring(CACHE_PREFIX.length) : key;
 }
 
+const NB_MILLISEC_IN_ONE_DAY = 1000*60*60*24;
+
 const getEntryFromCache = (key) => {
     const entryFromStorage = localStorage.getItem(key);
     if (!entryFromStorage) return null
-    return JSON.parse(entryFromStorage);
+    const entry = JSON.parse(entryFromStorage);
+    if (Date.now() - entry.created > NB_MILLISEC_IN_ONE_DAY) return null;
+    return entry;
 }
 
 const isItOneOfOurKeys = (key) => {
@@ -20,14 +24,13 @@ const isItOneOfOurKeys = (key) => {
 
 export class OrgCheckDataCacheManager {
     has(key) {
-        return localStorage.getItem(generatePhysicalKey(key)) !== null;
+        return getEntryFromCache(generatePhysicalKey(key)) !== null;
     }
     get(key) {
         const entry = getEntryFromCache(generatePhysicalKey(key));
         if (!entry) return null;
         if (entry.type === 'map') return new Map(entry.data);
         return entry.data;
-
     }
     set(key, value) {
         try {
