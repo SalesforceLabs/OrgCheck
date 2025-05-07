ac09b26f570b495ae36a2da80516ed931160cb99
Vincent FINET <vfinet@salesforce.com>
2025-05-07 11:45:28 +0200

    my domais should be ok #537

--- build/src/api/core/orgcheck-api-codescanner.js
+++ build/src/api/core/orgcheck-api-codescanner.js
@@ -1,4 +1,4 @@
-const REGEX_COMMENTS_AND_NEWLINES = new RegExp('(\\/\\*[\\s\\S]*?\\*\\/|\\/\\/.*\\n|\\n)', 'gi');
+const REGEX_COMMENTS_AND_NEWLINES = new RegExp('(\\/\\*[\\s\\S]*?\\*\\/|\\/\\/.*\\n|\\/\\/[^\\n]*|\\n)', 'gi');
 const REGEX_ISINTERFACE = new RegExp("(?:public|global)\\s+(?:interface)\\s+\\w+(\\s+(?:extends)\\s+\\w+)?\\s*\\{", 'i');
 const REGEX_ISENUM = new RegExp("(?:public|global)\\s+(?:enum)\\s+\\w+\\s*\\{", 'i');
 const REGEX_ISTESTSEEALLDATA = new RegExp("@IsTest\\s*\\(.*SeeAllData=true.*\\)", 'i');
@@ -7,7 +7,8 @@ const REGEX_HARDCODEDURLS = new RegExp("([A-Za-z0-9-]{1,63}\\.)+[A-Za-z]{2,6}", 
 const REGEX_HARDCODEDIDS = new RegExp("[,\"'\\s][a-zA-Z0-9]{5}0[a-zA-Z0-9]{9}([a-zA-Z0-9]{3})?[,\"'\\s]", 'ig');
 const REGEX_HASSOQL = new RegExp("\\[\\s*(?:SELECT|FIND)");
 const REGEX_HASDML = new RegExp("(?:insert|update|delete)\\s*(?:\\s\\w+|\\(|\\[)");
-const SALESFORCE_DOMAINS = ['salesforce.com', '.force.'];
+const SALESFORCE_DOMAINS = [ 'salesforce.com', '.force.' ];
+const SALESFORCE_MY_DOMAIN = '.my.salesforce.com';
 
 /**
  * @description Code Scanner class
@@ -30,14 +31,16 @@ export class CodeScanner {
         return sourceCode?.match(REGEX_HARDCODEDURLS) // extract the domains
             ?.filter((domain) => SALESFORCE_DOMAINS.findIndex((sfdomain) => domain.indexOf(sfdomain) >= 0) >= 0)  // filter only the salesforce domains
             .sort() // sorting the domains (if any)
-            .filter((e, i, s) => i === s.indexOf(e)); // unique domains
+            .filter((e, i, s) => i === s.indexOf(e)) // unique domains
+            .filter((domain) => domain.indexOf(SALESFORCE_MY_DOMAIN) < 0); // remove the my.salesforce.com domains
     }
 
     static FindHardCodedIDs(sourceCode) {
         return sourceCode?.match(REGEX_HARDCODEDIDS) // extract the salesforce ids
             ?.map(id => id?.substring(1, id?.length-1)) // remove the surrounding quotes or so
             .sort() // sorting the domains (if any)
-            .filter((e, i, s) => i === s.indexOf(e)); // unique domains
+            .filter((e, i, s) => i === s.indexOf(e)) // unique domains
+            || []; // empty array if no ids found
     }
 
     static IsTestSeeAllData(sourceCode) {
