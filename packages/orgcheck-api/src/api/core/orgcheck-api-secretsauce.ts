ccddbd5a41304b9645d2697de175e0de913edc35
Vincent FINET <vfinet@salesforce.com>
2026-08-21 10:35:15 +0200

    Removing the comments about hasMfaByPass #742

--- packages/orgcheck-api/src/api/core/orgcheck-api-secretsauce.ts
+++ packages/orgcheck-api/src/api/core/orgcheck-api-secretsauce.ts
@@ -695,10 +695,8 @@ const ALL_SCORE_RULES: ScoreRule[] = [
         category: SCORE_RULE_CATEGORIES.USER_ADOPTION
     }, {
         id: 72, 
-        // hasMfaByPass was removed because Salesforce retired the user permission called "Bypass Multi-Factor Authentication for User Interface Logins". 
-        // So we will not use it anymore in this rule.
         description: 'User is logging directly without MFA',
-        formula: ((d: SfdcUser) => d.nbDirectLoginsWithoutMFA > 0 /*&& d.hasMfaByPass !== true*/) as (data: unknown) => boolean,
+        formula: ((d: SfdcUser) => d.nbDirectLoginsWithoutMFA > 0) as (data: unknown) => boolean,
         errorMessage: `This user is logging in directly to Salesforce without using MFA (Multi-Factor Authentication). Please work with your user to make them use MFA for better security.`,
         badField: 'nbDirectLoginsWithoutMFA',
         applicable: [ DataAliases.SfdcUser ],
